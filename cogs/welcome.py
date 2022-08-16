9311913aa83762e01e2d5a74cf1a58c419a26aab
Kanav Arora <69228499+realkanavarora@users.noreply.github.com>
2022-08-16 23:40:53 +0530

    Update welcome.py

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -14,7 +14,7 @@ class WelcomeModule(commands.Cog, name='Developer Commands'):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        Family_Role= []
+        Family_Role= [985583040103321610,985583064677769288]
         channel = self.bot.get_channel(985583159586459708)
         em = embed=discord.Embed(description="""𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐭𝐨 𝐍ι𝐠нτ 𝐒ταℓκєяѕ<:tsc_smileykiss:986312710541168700>\n \n<a:tsc_love:997038267096649808> ʙᴇ ꜱᴜʀᴇ ᴛᴏ ᴄʜᴇᴄᴋ ᴏᴜᴛ\n<a:arrow_blue:997038188931596338> <#985583140758249472>\n<a:arrow_blue:997038188931596338> <#985583142859571250>\n<a:AUIHug:997529641575251988> ʜᴏᴘᴇ ʏᴏᴜ ᴇɴᴊᴏʏ ꜱᴛᴀʏɪɴɢ ʜᴇʀᴇ <a:tsc_heartlike:997515597619409037>""",timestamp=datetime.datetime.utcnow(), color=0xff0000)
         embed.set_author(name=f"{member.name}", icon_url=f"{member.avatar_url}")
