eeac39f2e5101299caa6dcd5b043787a6c4cfb9d
Lakshh Chopra <86960331+LakshyaChopra009@users.noreply.github.com>
2022-10-02 11:47:55 +0530

    Update welcome.py

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -2,6 +2,7 @@ from sqlite3 import Timestamp
 import discord
 from discord.ext import commands
 import datetime
+import asyncio 
 
 class WelcomeModule(commands.Cog, name='Developer Commands'):
     '''These are the developer commands'''
@@ -21,7 +22,9 @@ class WelcomeModule(commands.Cog, name='Developer Commands'):
         embed.set_author(name=f"{member.name}", icon_url=f"{member.avatar_url}")
         embed.set_image(url="https://media.discordapp.net/attachments/985926436760191106/997092526928437258/welcome_banner_2.gif")
         embed.set_footer(text=f"You are our {member.guild.member_count} member",icon_url=member.guild.icon_url)
-        await channel.send(f"𝐖𝐞𝐥𝐜𝐨𝐦𝐞 {member.mention} <a:tsc_thanks:986312698398650450>", embed=em, delete_after=10.0)
+        await channel.send(f"𝐖𝐞𝐥𝐜𝐨𝐦𝐞 {member.mention} <a:tsc_thanks:986312698398650450>", embed=em)
+        await asyncio.sleep(60) # waiting 60 seconds
+        await msg.delete() # Deleting msg
         for i in Family_Role: 
             await member.add_roles(member.guild.get_role(i))
 
