d7d129fbf5153618a988381df34ed65f12b36613
Lakshh Chopra <86960331+LakshyaChopra009@users.noreply.github.com>
2022-11-21 16:37:47 +0530

    Update welcome.py

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -15,7 +15,7 @@ class WelcomeModule(commands.Cog, name='Developer Commands'):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         Family_Role= [1025734170405765180,1025734224323543100,1025734214659866644,1025734215662321684,1025734216153042975]
-        channel = self.bot.get_channel(1043446271584764034)
+        channel = self.bot.get_channel(1043481677210603520)
         em = embed=discord.Embed(description="""𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐭𝐨 𝐍ι𝐠нτ 𝐒ταℓκєяѕ<:tsc_smileykiss:986312710541168700>\n \n<a:tsc_love:997038267096649808> ʙᴇ ꜱᴜʀᴇ ᴛᴏ ᴄʜᴇᴄᴋ ᴏᴜᴛ\n<a:arrow_blue:997038188931596338> <#985583140758249472>\n<a:arrow_blue:997038188931596338> <#985583142859571250>\n<a:AUIHug:997529641575251988> ʜᴏᴘᴇ ʏᴏᴜ ᴇɴᴊᴏʏ ꜱᴛᴀʏɪɴɢ ʜᴇʀᴇ <a:tsc_heartlike:997515597619409037>""",timestamp=datetime.datetime.utcnow(), color=0x7105D0)
         em.set_thumbnail(url="https://media.discordapp.net/attachments/1021751484112703509/1025104623381516358/dilliwali-namaste-ji.gif")
         embed.set_author(name=f"{member.name}", icon_url=f"{member.avatar_url}")
