805814a345fb555bd078c5604d36fee1384997fa
Lakshh Chopra <86960331+LakshyaChopra009@users.noreply.github.com>
2023-08-03 15:40:25 +0530

    Update welcome.py

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -27,7 +27,7 @@ class WelcomeModule(commands.Cog, name='Developer Commands'):
 
     @commands.command()
     async def testgreet(self,ctx):
-        channel = self.bot.get_channel(1126564250064801793)
+        channel = self.bot.get_channel(1126564306046169138)
         em = embed=discord.Embed(description="""𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐭𝐨  𝐆υρѕнυρ <a:zz_cheers:1136590964199272458>\n \n<a:zz_cute_love:1126911156095815730> ʙᴇ ꜱᴜʀᴇ ᴛᴏ ᴄʜᴇᴄᴋ ᴏᴜᴛ\n<a:zz_blue_arrow:1126911141952630845> <#1126564277701066793>\n<a:zz_blue_arrow:1126911141952630845> <#1126564280343461898>\n<a:zz_mocha_hug:1126911149724672051> ʜᴏᴘᴇ ʏᴏᴜ ᴇɴᴊᴏʏ ꜱᴛᴀʏɪɴɢ ʜᴇʀᴇ <a:zz_heartlike:1126911146981609493>""",timestamp=datetime.datetime.utcnow(), color=0x7105D0)
         em.set_thumbnail(url="https://media.discordapp.net/attachments/1021751484112703509/1025104623381516358/dilliwali-namaste-ji.gif")
         embed.set_author(name=f"{member.name}", icon_url=f"{member.avatar_url}")
